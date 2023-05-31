29602aa4f549b8ee29fe1954313f762404d08e6e
renu <renu_rll@163.com>
2023-05-31 21:10:50 +0800

    add R uniform return type

---
+++ src/main/java/com/example/psychologybackend/utils/R.java
@@ -0,0 +1,58 @@
+package com.example.psychologybackend.utils;
+
+import lombok.Data;
+import org.apache.http.HttpStatus;
+
+@Data
+public class R<T> {
+    private String msg;
+    private Integer code;
+    private  T data;
+
+
+    public  static R ok(){
+        R r = new R();
+        r.code = HttpStatus.SC_OK;
+        r.msg = "success";
+        return r;
+    }
+
+    public static<T> R ok(String msg){
+        R r = new R();
+        r.code = HttpStatus.SC_OK;
+        r.msg = msg;
+        return r;
+    }
+
+    public static<T> R ok(T data){
+        R<T> r = new R<T>();
+        r.code = HttpStatus.SC_OK;
+        r.msg = "sucess";
+        r.data = data;
+        return r;
+    }
+
+    public static R error(int code,String msg){
+        R r = new R();
+       r.code =code;
+       r.msg = msg;
+        return  r;
+
+    }
+
+    public static R error(String msg){
+        // 固定的状态吗500
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR,msg);
+    }
+
+    public static R error(){
+        // 固定的状态吗500
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR,"未知异常，请联系管理员");
+    }
+
+
+
+
+
+
+}
